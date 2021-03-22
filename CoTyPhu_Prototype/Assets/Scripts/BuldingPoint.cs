3fc53b727b657855b8c6722ce948c1f3f67a62e2
UIT-SE-TrungVo <66295737+UIT-SE-TrungVo@users.noreply.github.com>
2021-03-22 15:24:16 +0700

    Make House & Build Mechanism

---
+++ CoTyPhu_Prototype/Assets/Scripts/BuldingPoint.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuldingPoint : MonoBehaviour
+{
+    [SerializeField] GameObject[] arrHouses;
+    int currentHouseID; //id == -1 means no house is built on the point
+    GameObject currentHouse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    int GetKeyNumber()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0)) return 0;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) return 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) return 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) return 3;
+        if (Input.GetKeyDown(KeyCode.Alpha4)) return 4;
+        return currentHouseID;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //demo: make key_catch to build house
+        int newHouse = GetKeyNumber();
+        if (newHouse != currentHouseID)
+        {
+            if (currentHouse != null) Destroy(currentHouse.gameObject);
+            currentHouse = Instantiate(arrHouses[newHouse], this.transform.position, Quaternion.identity);
+            currentHouseID = newHouse;
+        }
+    }
+}
