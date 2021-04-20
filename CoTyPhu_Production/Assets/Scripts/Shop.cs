87639f5d057a94ca1891c89c8db5e1d1d7f2729e
Luong Ly Cong Thang <57753987+thangluong2202@users.noreply.github.com>
2021-04-20 16:20:48 +0700

    create class for item and status

---
+++ CoTyPhu_Production/Assets/Scripts/Shop.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Code cac chuc nang cua giao dien cua hang
+/// </summary>
+public class Shop
+{
+	//  Events ----------------------------------------
+
+
+	//  Properties ------------------------------------
+	public List<BaseItem> ItemPool
+	{
+		get { return _itemPool; }
+		//set { _itemPool = value; }
+	}
+
+	public List<BaseItem> ItemInShop
+    {
+		get { return _itemInShop; }
+		//set { _itemInShop = value; }
+	}
+
+	public int MaxSellItem
+	{
+		get { return _maxSellItem; }
+		//set { _maxSellItem = value; }
+	}
+
+	//  Fields ----------------------------------------
+	private List<BaseItem> _itemPool;
+	private List<BaseItem> _itemInShop;
+	private int _maxSellItem;
+
+	//  Initialization --------------------------------
+	public Shop()
+	{
+
+	}
+
+	//  Methods ---------------------------------------
+	public BaseItem GetRandomItem()
+	{
+		return null;
+	}
+
+	public bool AddItemToPool(BaseItem item)
+	{
+		return true;
+	}
+
+	public bool RemoveItemFromPool(int index)
+    {
+		return true;
+    }
+
+	public bool AddItemToShop(BaseItem item)
+	{
+		return true;
+	}
+
+	public bool RemoveItemFromShop(int index)
+    {
+		return true;
+    }
+
+	//  Event Handlers --------------------------------
+
+}
